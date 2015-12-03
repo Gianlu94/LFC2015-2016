ec4bdf16bf92e6a891644e3d4009d9cdc636d104
gianLuke <sanar@live.it>
2015-12-03 10:50:35 +0100

    Fix vari

--- 1 - lex introduction/Lez_4/Homework/H1/Versione2/lex.yy.c
+++ 1 - lex introduction/Lez_4/Homework/H1/Versione2/lex.yy.c
@@ -368,8 +368,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 5
-#define YY_END_OF_BUFFER 6
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -377,9 +377,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[9] =
+static yyconst flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    6,    3,    4,    2,    1,    0
+        0,    0,    5,    2,    3,    2,    1,    1,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -416,27 +416,29 @@ static yyconst flex_int32_t yy_ec[256] =
 
 static yyconst flex_int32_t yy_meta[5] =
     {   0,
-        1,    1,    1,    1
+        1,    1,    1,    2
     } ;
 
-static yyconst flex_int16_t yy_base[9] =
+static yyconst flex_int16_t yy_base[11] =
     {   0,
-        0,    0,    5,    6,    6,    6,    6,    6
+        0,    0,    6,    7,    7,    0,    0,    0,    7,    3
     } ;
 
-static yyconst flex_int16_t yy_def[9] =
+static yyconst flex_int16_t yy_def[11] =
     {   0,
-        8,    1,    8,    8,    8,    8,    8,    0
+        9,    1,    9,    9,    9,   10,   10,   10,    0,    9
     } ;
 
-static yyconst flex_int16_t yy_nxt[11] =
+static yyconst flex_int16_t yy_nxt[12] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8,    8
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
+        9
     } ;
 
-static yyconst flex_int16_t yy_chk[11] =
+static yyconst flex_int16_t yy_chk[12] =
     {   0,
-        1,    1,    1,    1,    3,    8,    8,    8,    8,    8
+        1,    1,    1,    1,   10,    3,    9,    9,    9,    9,
+        9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -453,38 +455,10 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "telefono1.l"
-#line 2 "telefono1.l"
-	char telefono [10]; 
-	int countT = 0;
-	int countN = 0;
-	int finish = 0;
-	void riinitA(char * telefono){
-		/*int i;
-		for (i = 0; i < 10; i++){
-			telefono[i]='0';		
-		}
-		*/
-		//char nuovo [10];
-		//telefono = nuovo;	
-	}
-	int inizializza (int * c1, int * c2){
-		*c1 = 0;
-		*c2 = 0;	
-	}
-	int check1 (int c1,int c2){
-		if ((c1 > 8)||(c2 > 2))
-			return 1;
-		else
-			return 0;
-	}
-	int check2 (int c1,int c2){
-		if ((c1 == 8)&&((c2 == 0)||(c2==1)||(c2==2)))
-			return 0;
-		else 
-			return 1;
-	}
-#line 488 "lex.yy.c"
+#line 1 "div_73_v2.l"
+#line 2 "div_73_v2.l"
+
+#line 462 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -671,9 +645,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 33 "telefono1.l"
+#line 5 "div_73_v2.l"
 
-#line 677 "lex.yy.c"
+#line 651 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -726,13 +700,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -758,64 +732,28 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 34 "telefono1.l"
-{	
-			telefono[countN+countT]=yytext[0];
-			printf("N %d D %c telefono %s\n",countN+countT,yytext[0],telefono);
-			countN++;
-			if (check1(countN,countT)==1){
-				riinitA(telefono);
-				inizializza(&countT,&countN);
-				printf(" chiamatoN1");
-			}
-			else if (check2(countN,countT)==0){
-					printf("Telefono : %s\n",telefono);
-					riinitA(telefono);
-					inizializza(&countT,&countN);
-					printf(" chiamatoN2");
-			}
-			
-		}
+#line 6 "div_73_v2.l"
+{if ((atoi(yytext) % 7) == 0){
+			printf("Numero = %d -- %d\n",atoi(yytext),atoi(yytext)+3);
+		};}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 52 "telefono1.l"
-{	int pos = countN+countT;
-			if ((pos != 0) && (pos!=9)){
-				telefono[pos]=yytext[0];
-				printf("T %d D %c telefono %s\n",countN+countT,yytext[0],telefono);
-				countT++;
-				if (check1(countN,countT)==1){
-					riinitA(telefono);
-					inizializza(&countT,&countN);
-					printf(" chiamatoT1");
-				}
-				else if (check2(countN,countT)==0){
-						printf("Telefono : %s\n",telefono);
-						riinitA(telefono);
-						inizializza(&countT,&countN);
-						printf(" chiamatoT2");
-				}
-			}
-		}
+#line 9 "div_73_v2.l"
+
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 70 "telefono1.l"
-{inizializza(&countT,&countN);riinitA(telefono);}
+#line 10 "div_73_v2.l"
+
 	YY_BREAK
 case 4:
-/* rule 4 can match eol */
 YY_RULE_SETUP
-#line 71 "telefono1.l"
-{inizializza(&countT,&countN);riinitA(telefono);}
-	YY_BREAK
-case 5:
-YY_RULE_SETUP
-#line 73 "telefono1.l"
+#line 11 "div_73_v2.l"
 ECHO;
 	YY_BREAK
-#line 819 "lex.yy.c"
+#line 757 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1107,7 +1045,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1135,11 +1073,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 9);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1813,10 +1751,10 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 73 "telefono1.l"
+#line 11 "div_73_v2.l"
 
 
-int main(){
+main(){
 	yylex();
 	return 0;
 }
